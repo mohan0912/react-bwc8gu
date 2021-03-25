f8205687b23c78c52c177738e9541deb55adcfcb
mohan0912 <mohan1995pattnaik@gmail.com>
2021-03-25 11:38:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bwc8gu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bwc8gu)
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import SignUp from "./SignUp";
+
+ReactDOM.render(
+  <>
+    <Header />
+    <SignUp />
+  </>,
+  document.getElementById("root")
+);
